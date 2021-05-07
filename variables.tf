8f90320c51bbdd1678237c03148c15facafdcbf6
PolyakovVictor <82318983+PolyakovVictor@users.noreply.github.com>
2021-05-07 18:09:26 +0300

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {default = "eu-central-1"}
+
+variable "access_key" {default = ""}
+
+variable "secret_key" {default = ""}
+
+variable "allow_ports" {default = ["443","80"]}
+
+variable "cidr_block_for_all" {default = ["0.0.0.0/0"]}
+
+variable "instance_type" {default = "t3.micro"}
